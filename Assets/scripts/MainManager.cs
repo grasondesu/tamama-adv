5754cb31866e2887cb55d99c4a32f08dce665ec8
grason <guragamemaking@gmail.com>
2025-05-18 21:48:52 +0900

    諸々の変更完了

---
+++ Assets/scripts/MainManager.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+using UnityEngine;
+
+public class MainManager : MonoBehaviour
+{
+    [SerializeField, Header("ゲームオーバーUI")]
+    private GameObject gameOverUI;
+
+    [SerializeField, Header("ゲームクリアUI")]
+    private GameObject gameClearUI;
+
+    private GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = FindObjectOfType<PlayerController>().gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    { 
+        ShowGameOverUI();
+    }
+
+    private void ShowGameOverUI()
+    {
+        if (player != null) return;
+
+        gameOverUI.SetActive(true);
+    }
+
+    public void ShowGameClearUI()
+    {
+        gameClearUI.SetActive(true);
+    }
+}
